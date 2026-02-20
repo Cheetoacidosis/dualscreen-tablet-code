41cc472784289187c3ede2eea7d6d8c53478eb58
RPi Zero 2W <coordinator@gas.usu.edu>
2026-02-20 23:16:42 +0000

    fooled around with i2c a bit. Not getting any response back from the chip other than 0

--- touch_panel/testing_i2c.py
+++ touch_panel/testing_i2c.py
@@ -1,13 +1,30 @@
 # I2C library
-import smbus
+import smbus2
 from gpiozero import Device, DigitalOutputDevice, InputDevice
 import time
 
+FIRMWARE_REG = 0x8144
+TOUCH_NUMBER_REG = 0x804c
+COMMAND_STATUS_REG = 0x81A8
+COMMAND_CHECK_REG = 0x8046
+COMMAND_REG = 0x8040
+
+CONFIG_CHKSUM_REG = 0x80FF
+CONFIG_FRESH_REG = 0x8100
+
+COORD_STATUS_REG = 0x814E
+
+ENTER_RECEIVE_MODE = 0x22
+
+
 #  GT911 reported 7-bit address 0x5d
-touch_address = 0x5d
+# 8bit write address 0xBA
+# 8bit read address  0xBB
+# ADDR = 0x5d
+ADDR = 0x5d
 
 # Initialize I2C (SMBus)
-bus = smbus.SMBus(1) #GPIO
+i2c = smbus2.SMBus(1)
 
 int_pin = 18
 rst_pin = 26
@@ -16,13 +33,30 @@ rst_dev = DigitalOutputDevice(pin=rst_pin, initial_value=True)
 int_dev = DigitalOutputDevice(pin=int_pin, initial_value=True)
 
 
-# Try changing the address of the doodad
+# See if we can make any changes at fucking all
 rst_dev.off()
-int_dev.off()
-
 time.sleep(1)
 
-rst_dev.on()
+CSR_second = i2c.read_byte_data(ADDR, 0x814e)
+
+print(CSR_second)
+
+# touchnum = i2c.read_byte_data(ADDR, TOUCH_NUMBER_REG)
+# print(touchnum)
+
+# firm_low = i2c.read_byte_data(ADDR, FIRMWARE_REG)
+# firm_high = i2c.read_byte_data(ADDR, FIRMWARE_REG + 1)
+
+# print(firm_low)
+# print(firm_high)
+
+# Try changing the address of the doodad
+# rst_dev.off()
+# int_dev.off()
+
+# time.sleep(1)
+
+# rst_dev.on()
 
 # rst_dev.off()
 # int_dev.on()
