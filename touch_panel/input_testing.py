07f51eb7029fb436f12245ab5e46573e3fa5a2ab
CallieK <callie.kubesh@usu.edu>
2026-03-28 19:56:03 -0600

    Simulate a single finger drawing a circle through evdev

--- touch_panel/input_testing.py
+++ touch_panel/input_testing.py
@@ -1,55 +1,90 @@
-# from evdev import UInput, AbsInfo, ecodes as e
-# import evdev
-# import time
-
-# ui = UInput()
-# while(True):
-#     ui.write(e.EV_KEY, e.KEY_A, 1)  # KEY_A down
-#     ui.write(e.EV_KEY, e.KEY_A, 0)  # KEY_A up
-#     ui.syn() #Events are queued until a syncronization event is recieved with .syn()
-#     time.sleep(1.1)
-
-# ui.close()
-
-# print (evdev.list_devices())
-# devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-# for device in devices:
-#     print(device.path, device.name, device.phys)
-
-# ui.close()
 
 
-# from evdev import UInput, AbsInfo, ecodes as e
-
-# cap = {
-#     # e.EV_SYsN : 0,
-#     e.EV_KEY : [e.KEY_A, e.KEY_B, e.BTN_LEFT, e.BTN_MOUSE, e.BTN_MIDDLE, e.BTN_RIGHT],
-#     e.EV_ABS : [
-#         (e.ABS_X, AbsInfo(value=0, min=0, max=255,
-#                           fuzz=0, flat=0, resolution=0)),
-#         (e.ABS_Y, AbsInfo(0, 0, 255, 0, 0, 0)),
-#         (e.ABS_MT_POSITION_X, AbsInfo(value=0, min=0, max=2940, fuzz=0, flat=0, resolution=31)) ]
         
-#     # ('EV_KEY', 1): [(('BTN_LEFT', 'BTN_MOUSE'), 272), ('BTN_RIGHT', 273), ('BTN_MIDDLE', 274), ('BTN_SIDE', 275), ('BTN_EXTRA', 276), ('BTN_FORWARD', 277), ('BTN_BACK', 278), ('BTN_TASK', 279) ]
-# }
-
-
-
-
 """
-Minimal working example of relative mouse movement
+Emulate a touch panel with a single point. Draws a circle every second
+For Qt to recognize it, you need to add the command-line argument
+"-plugin EvdevTouch:/dev/input/eventX", where X depends on your local HW setup.
+This would not be necessary if I knew the secret sauce required for Qt to realize this is a touch device
+
+Additionally, all widgets that are to be touch-accessible need to have specific attribute
+example:
+ui->pdfViewWidget->setAttribute(Qt::WA_AcceptTouchEvents, true);
 """
 from evdev import UInput, AbsInfo, ecodes as e
 import evdev
 import time
+import numpy as np
 
-cap = {e.EV_REL: [e.REL_X, e.REL_Y],e.EV_KEY: [e.BTN_LEFT]} 
+# Radius and center point for drawing a circle with the emulated touch panel
+p = (1128, 752)
+r = 250
+
+cap = {
+    e.EV_KEY : [e.KEY_A, e.KEY_B, e.BTN_LEFT, e.BTN_MOUSE, e.BTN_MIDDLE, e.BTN_RIGHT],
+    e.EV_ABS : [
+        (e.ABS_X, AbsInfo(value=0, min=0, max=2256, fuzz=0, flat=0, resolution=31)),
+        (e.ABS_Y, AbsInfo(0, 0, 1504, 0, 0, 31)),
+        (e.ABS_MT_SLOT, AbsInfo(value=0, min=0, max=10, fuzz=0, flat=0, resolution=0)),
+        (e.ABS_MT_POSITION_X, AbsInfo(value=0, min=0, max=2256, fuzz=0, flat=0, resolution=31)),
+        (e.ABS_MT_POSITION_Y, AbsInfo(value=0, min=0, max=1504, fuzz=0, flat=0, resolution=31)),
+        (e.ABS_MT_TRACKING_ID, AbsInfo(value=0, min=0, max=9999, fuzz=0, flat=0, resolution=0)) ]
+}
 
 ui = UInput(cap, name='example-device', version=0x3)
 
-# move mouse cursor
+# Enough time to get away lol
+time.sleep(5)
+
+# move touch 
 while(True):
-    ui.write(e.EV_REL, e.REL_X, 20)
-    ui.write(e.EV_REL, e.REL_Y, 20)
+    # Initialize new touch event
+    ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
+    ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, 45) # Can be anything, as long as each touch is unique
+    # ui.write(e.EV_KEY, e.BTN_LEFT, 1) # "Pen down"
     ui.syn()
-    time.sleep(0.05)
+
+    # Draw a circle
+    for i in range(314*2):
+        x = int(r*np.cos(i/100) + p[0])
+        y = int(r*np.sin(i/100) + p[1])
+
+        ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
+        ui.write(e.EV_ABS, e.ABS_MT_POSITION_X, x)
+        ui.write(e.EV_ABS, e.ABS_MT_POSITION_Y, y)
+        ui.syn()
+
+    time.sleep(0.01)
+    # End touch event
+    ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
+    ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, -1)
+    # ui.write(e.EV_KEY, e.BTN_LEFT, 0) # "pen up"
+    ui.syn()
+    time.sleep(1)
+
+
+
+
+
+
+
+
+
+if (False):
+    """
+    Minimal working example of relative mouse movement
+    """
+    from evdev import UInput, AbsInfo, ecodes as e
+    import evdev
+    import time
+
+    cap = {e.EV_REL: [e.REL_X, e.REL_Y],e.EV_KEY: [e.BTN_LEFT]} 
+
+    ui = UInput(cap, name='example-device', version=0x3)
+
+    # move mouse cursor
+    while(True):
+        ui.write(e.EV_REL, e.REL_X, 20)
+        ui.write(e.EV_REL, e.REL_Y, 20)
+        ui.syn()
+        time.sleep(0.05)
