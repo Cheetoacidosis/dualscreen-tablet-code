2686ceff98cdab040e733708fa48fa4e87b08660
CallieK <callie.kubesh@usu.edu>
2026-03-28 20:23:34 -0600

    the beginnings of an actual linux driver

---
+++ touch_panel/send_touch_to_linux.py
@@ -0,0 +1,53 @@
+"""
+(Untested 3/28)
+Should send touch inputs from the GT911 to the linux userspace
+"""
+
+import os
+import time
+from gt911_panel import GT911_Panel
+from evdev import UInput, AbsInfo, ecodes as e
+import evdev
+import time
+
+# Connect to the first panel
+panel1 = GT911_Panel(i2c_address = 0x14, int_pin = 18, rst_pin = 26)
+if not panel1.connect():
+    print("Failed connection P1")
+    input("Press enter to continue")
+panel1.start_reading()
+
+
+# Set up the touch emulation device
+cap = {
+    e.EV_KEY : [e.KEY_A, e.KEY_B, e.BTN_LEFT, e.BTN_MOUSE, e.BTN_MIDDLE, e.BTN_RIGHT],
+    e.EV_ABS : [
+        (e.ABS_X, AbsInfo(value=0, min=0, max=2256, fuzz=0, flat=0, resolution=31)),
+        (e.ABS_Y, AbsInfo(0, 0, 1504, 0, 0, 31)),
+        (e.ABS_MT_SLOT, AbsInfo(value=0, min=0, max=10, fuzz=0, flat=0, resolution=0)),
+        (e.ABS_MT_POSITION_X, AbsInfo(value=0, min=0, max=2256, fuzz=0, flat=0, resolution=31)),
+        (e.ABS_MT_POSITION_Y, AbsInfo(value=0, min=0, max=1504, fuzz=0, flat=0, resolution=31)),
+        (e.ABS_MT_TRACKING_ID, AbsInfo(value=0, min=0, max=9999, fuzz=0, flat=0, resolution=0)) ]
+}
+ui = UInput(cap, name='send_touch_to_linux', version=0x3)
+
+
+# Run foreveh
+while (True):
+    # Only tracks one input rn
+    if (panel1.fresh == True):
+        (x1, y1) = panel1.coords()
+
+        # Close touch event
+        if (x1.empty() == True):
+            ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
+            ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, -1) # Can be anything, as long as each touch is unique
+            ui.syn()
+            
+        # Update touch event / create new touch event
+        else:
+            ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
+            ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, 45) # Can be anything, as long as each touch is unique
+            ui.write(e.EV_ABS, e.ABS_MT_POSITION_X, x1[0])
+            ui.write(e.EV_ABS, e.ABS_MT_POSITION_Y, y1[0])
+            ui.syn()
