682b9d7378e944a9af7ae5b7330c0f7e14a5357a
RPi Zero 2W <coordinator@gas.usu.edu>
2026-04-02 23:23:38 +0100

    touch is kinda working. The previous input streaks together into the newest one, but its not aweful

--- touch_panel/send_touch_to_linux.py
+++ touch_panel/send_touch_to_linux.py
@@ -35,33 +35,73 @@ cap = {
 }
 ui = UInput(cap, name='send_touch_to_linux', version=0x3)
 
+# print(type(panel1.coords()))
+
+# Initialize
+touch_id = panel1.track_ids
 
 # Run foreveh
 while (True):
     # Only tracks one input rn
     if (panel1.fresh == True):
+        # print("panel fresh")
         # swap x and y
         (y1, x1) = panel1.coords()
+        old_touch_ids = touch_id
+        touch_id = panel1.track_ids
 
-        # Close touch event
-        if not x1:
+        # invert y
+        try:
+            y1[0] = SQUARE - y1[0]
+        except:
+            print("nothing to invert")
+
+        # print()
+
+        # if y1 and not oldy1
+            # new touch
+        # if not y1 and oldy1
+            # touch released
+        # if y1 != oldy1
+            # touch moved
+
+        if touch_id and not old_touch_ids:
+            # new touch
+            print("New touch")
+            ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
+            ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, 45) # Can be anything, as long as each touch is unique
+            ui.syn()
+
+            ui.write(e.EV_ABS, e.ABS_MT_POSITION_X, x1[0])
+            ui.write(e.EV_ABS, e.ABS_MT_POSITION_Y, y1[0])
+            ui.syn()
+
+        elif (not touch_id) and old_touch_ids:
+            # Touch released
+            print("touch released")
             ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
             ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, -1) # Can be anything, as long as each touch is unique
             ui.syn()
-            
-        # Update touch event / create new touch event
-        else:
+
+        elif touch_id != old_touch_ids:
+            # Touch moved
             ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
-            ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, 45) # Can be anything, as long as each touch is unique
             ui.write(e.EV_ABS, e.ABS_MT_POSITION_X, x1[0])
-    # Read data
-    # If fresh
-        # if empty
-            # set ID negavite
-        # if new touch down
-            # set "unique" ID (really just whatever the slot is)
-            # update evdev with the position
-        # else
-            # update evdev with the positionN_X, x1[0])
             ui.write(e.EV_ABS, e.ABS_MT_POSITION_Y, y1[0])
             ui.syn()
+
+        # Close touch event
+        # if not x1:
+        #     print("closing touch")
+        #     ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
+        #     ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, -1) # Can be anything, as long as each touch is unique
+        #     ui.syn()
+            
+        # # Update touch event / create new touch event
+        # else:
+        #     print("updating touch")
+        #     ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
+        #     ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, 45) # Can be anything, as long as each touch is unique
+        #     ui.write(e.EV_ABS, e.ABS_MT_POSITION_X, x1[0])
+        #     ui.write(e.EV_ABS, e.ABS_MT_POSITION_Y, y1[0])
+        #     ui.syn()
