fce4c78117459270aec18c1d2a3567bb4f8023df
RPi Zero 2W <coordinator@gas.usu.edu>
2026-04-10 22:37:58 +0100

    Organizing for creating a systemd service for the two panels

---
+++ starttouch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ./touch_panel/venv/bin/activate
+python ./touch_panel/send_touch_to_linux.py
--- touch_panel/drive_panel1.py
+++ touch_panel/drive_panel1.py
@@ -1,6 +1,5 @@
 """
-(Untested 3/28)
-Should send touch inputs from the GT911 to the linux userspace
+send touch inputs from the GT911 to the linux userspace
 """
 
 import os
@@ -15,7 +14,6 @@ SQUARE = 1024
 uuid = 1
 
 def ReleaseTouch(ui, MT_SLOT) -> None:
-    print("releasing touch ", MT_SLOT)
     ui.write(e.EV_ABS, e.ABS_MT_SLOT, MT_SLOT)
     ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, -1) # Can be anything, as long as each touch is unique
     ui.syn()
@@ -26,7 +24,6 @@ def StartTouch(ui, MT_SLOT, x, y) -> int:
     global uuid
     ID = uuid
     uuid = uuid + 1
-    print("starting touch ", MT_SLOT, " ID:", ID)
     # Swap x & y, then invert y
     t = y
     y = x
@@ -53,7 +50,6 @@ def StartTouch(ui, MT_SLOT, x, y) -> int:
 
 
 def UpdateTouch(ui, MT_SLOT, ID, x, y) -> None:
-    print("moving touch ", MT_SLOT)
     # Swap x & y, then invert y
     t = y
     y = x
@@ -74,11 +70,10 @@ panel1 = GT911_Panel(i2c_address = 0x5d, int_pin = 18, rst_pin = 26)
 
 while not panel1.connect():
     print("Failed connection Panel 1")
-    input("Press enter to try again")
+    time.sleep(0.3)
 
 time.sleep(1)
 panel1.start_reading()
-
 
 
 squareAbsInfo = AbsInfo(value=0, min=0, max=SQUARE, fuzz=0, flat=0, resolution=31)
@@ -94,27 +89,22 @@ cap = {
         (e.ABS_MT_POSITION_Y, squareAbsInfo),
         (e.ABS_MT_TRACKING_ID, AbsInfo(value=0, min=0, max=9999, fuzz=0, flat=0, resolution=0)) ]
 }
-ui = UInput(cap, name='send_touch_to_linux', version=0x3)
+ui = UInput(cap, name='Touch Panel 1', version=0x3)
 
-# print(type(panel1.coords()))
+
 print("starting")
-# print(panel1.connected)
+
 
 # Initialize
 touch_id = None
 TouchDict = dict() #"PanelTouchID" : "evdev_MT_slot"
 
-
-# Run foreveh
+# Send taps to Linux userspace
 while (True):
-
-
-
     if (panel1.fresh == True):
 
         # Nab our new data
         panel_data = panel1.coords()
-        # touch_id = panel1.track_ids
 
         for new_slot in panel_data:
             # Update the specified touch with new coords
@@ -130,59 +120,8 @@ while (True):
                 mt_track_id = StartTouch(ui, new_slot, panel_data[new_slot][0], panel_data[new_slot][1])
                 TouchDict[new_slot] = mt_track_id
 
-        # TouchDictCopy = TouchDict
         for old_touch_ID in list(TouchDict):
             # If old info is not seen in new list, we assume it's been removed
             if old_touch_ID not in panel_data:
                 ReleaseTouch(ui, old_touch_ID)
                 TouchDict.pop(old_touch_ID)
-
-
-
-
-
-
-        # print("panel fresh")
-        # swap x and y
-        # (y1, x1) = panel1.coords()
-        # old_touch_ids = touch_id
-        # touch_id = panel1.track_ids
-
-        # # print("fresh")
-
-
-        # # invert y
-        # try:
-        #     y1[0] = SQUARE - y1[0]
-        # except:
-        #     # print("nothing to invert")
-        #     nothing = None
-
-        
-
-        # if touch_id and not old_touch_ids:
-        #     # new touch
-        #     # print("New touch")
-        #     ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
-        #     ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, 45) # Can be anything, as long as each touch is unique
-        #     ui.syn()
-
-        #     ui.write(e.EV_ABS, e.ABS_MT_POSITION_X, x1[0])
-        #     ui.write(e.EV_ABS, e.ABS_MT_POSITION_Y, y1[0])
-        #     ui.syn()
-
-        # elif (not touch_id) and old_touch_ids:
-        #     # Touch released
-        #     print("touch released")
-        #     ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
-        #     ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, -1) # Can be anything, as long as each touch is unique
-        #     ui.syn()
-
-        # elif touch_id != old_touch_ids:
-        #     # Touch moved
-        #     ui.write(e.EV_ABS, e.ABS_MT_SLOT, 0)
-        #     ui.write(e.EV_ABS, e.ABS_MT_POSITION_X, x1[0])
-        #     ui.write(e.EV_ABS, e.ABS_MT_POSITION_Y, y1[0])
-        #     ui.syn()
-
-
