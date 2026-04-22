acac29ff4dc7025e0c1a786806e747b73798d61f
RPi Zero 2W <coordinator@gas.usu.edu>
2026-04-22 19:17:20 +0100

    script waits until touchscreen exists to begin

--- runscript.sh
+++ runscript.sh
@@ -11,7 +11,13 @@ loop(){
   loop $1
 }
 
-
+FILE=/dev/input/event2
+# Do nothing until the touchpanel is connected
+while [ ! -e "$FILE" ]
+do
+    echo "Waiting for touchscreen device to connect..."
+    sleep 0.25
+done
 
 sudo rm /dev/shm/display_data.bin
 sudo ./user_interface_test/v19/build/v19 -platform offscreen -plugin EvdevTouch:/dev/input/event2 &
